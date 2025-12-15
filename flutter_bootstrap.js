d5528e7b6bdfefbc1de5d9f8d8d4ec5029a21a28
qrlamir <qrlamir@gmail.com>
2025-12-15 23:02:45 +0000

    Initial deploy7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2115307704"
+    serviceWorkerVersion: "3604390650"
   }
 });
